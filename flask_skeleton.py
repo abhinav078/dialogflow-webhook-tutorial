968ffbac780ddc67550c653d02368cf0c1012996
abhinav078 <43104472+abhinav078@users.noreply.github.com>
2022-10-16 17:51:56 -0400

    Update flask_skeleton.py

--- flask_skeleton.py
+++ flask_skeleton.py
@@ -1,25 +1,20 @@
-# import flask dependencies
 from flask import Flask, request
+import json
 
 # initialize the flask app
 app = Flask(__name__)
 
-# create a route for webhook
+@app.route('/route')
+def route():
+  studentID = 200490537
+  value = {'StudentID' : studentID}
+  return json.dump(value)
+
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    req = request.get_json(silent=True, force=True)
-    fulfillmentText = ''
-    query_result = req.get('queryResult')
-    if query_result.get('action') == 'get.address':
-        ### Perform set of executable code
-        ### if required
-        ### ...
-
-        fulfillmentText = "Hi"
-    return {
-            "fulfillmentText": fulfillmentText,
-            "source": "webhookdata"
-        }
+  text = 'i have integrated with flask'
+  values = {'text':text}
+  return json.dump(values)
 
 # run the app
 if __name__ == '__main__':
